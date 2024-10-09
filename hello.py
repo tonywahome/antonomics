f3ca70d032bfa992e7f30fc20aa591256a4ffa76
tonywahome <a.wambugu@alustudent.com>
2024-10-09 10:39:31 +0200

    wifi lit

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-import sys
+fam = ['Dad', 1.82, 'Mom', 1.73, 'Carol', 1.68, 'Me', 1.84, 'Jay', 1.78]
+print(fam)
+fam_heights = fam + ['Tara', 1.30]
+print(fam_heights)
+del(fam_heights[7])
+print(fam_heights)
+list(fam_heights)
 
-
-def print_python_version():
-    print(sys.version)
---
+++ spiral.cmd
@@ -0,0 +1,29 @@
+position
+heading
+reset
+circle 20
+right 30
+circle 40
+right 30
+circle 60
+right 30
+circle 80
+right 30
+circle 100
+right 30
+circle 120
+right 30
+circle 140
+right 30
+circle 160
+right 30
+circle 180
+forward 100
+right 90
+forward 400
+right 90
+forward 500
+right 90
+forward 400
+right 90
+forward 300
